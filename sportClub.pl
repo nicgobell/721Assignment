e954e72591fef817c150e39aeadeb78b0a9694f8
KanyeWestBest <56983525+KanyeWestBest@users.noreply.github.com>
2019-10-25 19:12:17 -0400

    Update sportClub.pl

--- sportClub.pl
+++ sportClub.pl
@@ -18,12 +18,13 @@ elect([Arthur,Bart,Colleen,Dona,Eva,Frank]):-
     	pos(Arthur), pos(Bart), pos(Colleen), pos(Dona), pos(Eva), pos(Frank),
     	%restrict the values that wont happen
     	Arthur \= 2, %rule 1 (1/2)
+    Frank \= 4, Frank \=2, 
 	Bart \= 2, Bart \= 4, %rule 2
 	rule1(Bart, Arthur), %rule 1 (2/2)
+    rule6(Frank, Colleen),
 	rule3(Bart, Frank, Colleen), 
-	rule4(Donna, Eva, Frank), 
+	rule4(Dona, Eva, Frank), 
 	rule5(Eva, Arthur, Bart),
-	rule6(Frank, Colleen),
 % make sure 2 numbers are 0 and 1,2,3,4 are the rest. abuse finite domain.
         count(0,[Arthur,Bart,Colleen,Dona,Eva,Frank],No_Job), No_Job==2,
         count(1,[Arthur,Bart,Colleen,Dona,Eva,Frank],Prez), Prez==1,
@@ -52,38 +53,9 @@ rule4(D,0,0) :- D \= 0.
 rule5(E,0,0).
 rule5(0,A,B) :- A \= 0, B \= 0.
 
-rule6(1,C) :- C \= 2.
-rule6(F,C) :- F \= 1.
-
-
-
-
-
-
-/*	
-	Frank \= 2, Frank \= 4, Frank \= 4
-	Bart \= 2, Bart \= 4,
-	Arthur \= 2,
-
-
-	not(Frank==2), not(Frank==3), not(Frank==4),
-    	not(Bart==2),not(Bart==4),
-    	not(Arthur==2), 
-    	not((not(Eva==0),not(Bart==0),not(Arthur==0))),
-    	not((not(Donna==0),not(Frank==0))), not((not(Donna==0), not(Frank==0))),
-    	not((not(Colleen==0),not(Bart==0),Frank==0)),
-
-		% add the conditi
-   	
-		% true if bart and arthur are 0 or bart and arthur are numbers, or arthur is 0 and 
-		% bart is a number.
-    
-    	% make sure 2 numbers are 0 and 1,2,3,4 are the rest. abuse finite domain.
-    	count(0,[Arthur,Bart,Colleen,Dona,Eva,Frank],No_Job), No_Job==2,
-    	count(1,[Arthur,Bart,Colleen,Dona,Eva,Frank],Prez), Prez==1,
-    	count(2,[Arthur,Bart,Colleen,Dona,Eva,Frank],Vice), Vice==1,
-    	count(3,[Arthur,Bart,Colleen,Dona,Eva,Frank],Tres), Tres==1,
-    	count(4,[Arthur,Bart,Colleen,Dona,Eva,Frank],Sec), Sec==1.
-    	%Let us constrain the comain further
-*/ 
-incompatible(Bart,Arthur):-not(   (Bart==0, Arthur==0)).
+%rule6(1,C) :- C \= 2.
+%rule6(F,C) :- F \= 1.
+%rule6(Y,X):-Y==1,not(X==2).
+%rule6(Y,X):-Y==0.
+rule6(F,C):-F\=1.
+rule6(1,C):-C==0.
