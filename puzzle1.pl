5b0d013b604348fe702939d64374fbe46f6ab271
KanyeWestBest <56983525+KanyeWestBest@users.noreply.github.com>
2019-10-25 20:10:56 -0400

    Update puzzle1.pl

--- puzzle1.pl
+++ puzzle1.pl
@@ -1,3 +1,25 @@
+ all_diff([]).
+all_diff([N|L]):-not(member(N,L)), all_diff(L).
+
+dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6). dig(7). dig(8). dig(9).
+
+
+%addition, ?-check2([B,A,E,G,T,R]).
+
+
+check2([B,A,E,G,T,R]) :-
+    dig(B), B>0,
+    dig(G),G>0,
+    dig(A),dig(T),dig(R),dig(E),
+    
+   	E is (T*10+E)mod(10), C1 is (T*10+E)//10,
+    R is (E*10+B+C1)mod(10), C10 is (E*10+B+C1)//10,
+    A is (G*10+A+C10)mod(10), C100 is (G*10+A+C10)//10,
+    E is (G*10+B+C100)mod(10), B is (G+B+C100)//10,
+ 
+    all_diff([B,A,E,G,T,R]).
+
+%multiply, query: solve([Y,G,E,T,B,A,_,_,_,_]).
 solve(Z) :-
     assign(Z,[0,1,2,3,4,5,6,7,8,9]),
     check(Z).
@@ -26,30 +48,3 @@ permute([],[]).
 permute([X|K],P) :- 
     permute(K,L1),
     addany(X,L1,P).
-    
-    
-    %for the addition
-    %query check2([B,A,E,G,T,R]).
-
-    all_diff([]).
-all_diff([N|L]):-not(member(N,L)), all_diff(L).
-
-dig(0). dig(1). dig(2). dig(3). dig(4). dig(5). dig(6). dig(7). dig(8). dig(9).
-
-
-
-
-
-
-check2([B,A,E,G,T,R]) :-
-    dig(B), B>0,
-    dig(G),G>0,
-    dig(A),dig(T),dig(R),dig(E),
-    
-   	E is (T+E)mod(10), C1 is (T+E)//10,
-    R is (E+B+C1)mod(10), C10 is (E+B+C1)//10,
-    A is (G+A+C10)mod(10), C100 is (G+A+C10)//10,
-    E is (G*10+B+C100)mod(10), B is (G*10+B+C100)//10,
- 
-    all_diff([B,A,E,G,T,R]).
-    
